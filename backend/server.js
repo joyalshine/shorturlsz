98cb3e0a82f08685dfb4e91d976042b74c485885
Joyal Shine <101260206+joyalshine@users.noreply.github.com>
2024-06-22 03:53:34 +0530

    Combined both frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require("dotenv")
 const cors = require('cors');
@@ -5,13 +6,13 @@ dotenv.config()
 const { app, server } = require('./socket/socket');
 const PORT = process.env.PORT || 5000;
 
-
-
 const connectToDB = require('./db/connection')
 
 const chatRoutes = require('./routes/chatRoutes')
 const linkRoutes = require('./routes/linkRoutes')
 const noteRoutes = require('./routes/noteRoutes');
+
+const __dirnam = path.resolve()
 
 
 var corsOptions = {
@@ -26,6 +27,12 @@ app.use(cors(corsOptions));
 app.use("/chat",chatRoutes)
 app.use("/link",linkRoutes)
 app.use("/note",noteRoutes)
+
+app.use(express.static(path.join(__dirnam,"frontend/dist")))
+
+app.get("*",(req,res) => {
+  res.sendFile(path.join(__dirnam,"frontend","dist","index.html"))
+})
 
 server.listen(PORT,async () => {
     connectToDB()
